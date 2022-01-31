5c9538764fcdb409c8e856edd2b95c640d57f26c
cjgochanour <cjgochanour@gmail.com>
2022-01-30 20:47:54 -0600

    added logic to filter entries by students' instructor

--- src/components/roster/Entries.js
+++ src/components/roster/Entries.js
@@ -1,6 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getPendingEntries, getStudentProfiles } from "../ApiManager.js";
 
 export const Entries = () => {
-    const [entries, setEntries] = useState();
+    const [entries, setEntries] = useState([]);
+
+    useEffect(() => {
+        getPendingEntries().then((entriesArray) => {
+            getStudentProfiles().then((studentProfiles) => {
+                const entriesWithProfile = entriesArray.map((entry) => {
+                    entry.studentProfile = studentProfiles.find((sp) => sp.userId === entry.userId);
+                    return entry;
+                });
+                const filteredEntries = entriesWithProfile.filter(
+                    (entry) => entry.studentProfile?.instructorId === parseInt(localStorage.getItem("rude_user"))
+                );
+                setEntries(filteredEntries);
+            });
+        });
+    }, []);
+
     return <></>;
 };
