6cf84f701c9e68b802ba958634a49441266a1199
cjgochanour <cjgochanour@gmail.com>
2022-01-31 14:09:47 -0600

    pending entries now displays rudiment name - name is clickable and routes to the rudiment's details

--- src/components/requests/Entry.js
+++ src/components/requests/Entry.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Entry = ({ entry }) => {
     return (
         <li>
-            {entry.user.name} - {entry.bpm} BPM
+            {entry.user.name} - <Link to={`/library/${entry.rudimentId}`}>{entry.rudiment.name}</Link> - {entry.bpm} BPM
         </li>
     );
 };
