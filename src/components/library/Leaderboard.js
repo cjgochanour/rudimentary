c80f6e335f0c5314fed60a04ed17fbf29a6599ed
cjgochanour <cjgochanour@gmail.com>
2022-02-02 09:35:30 -0600

    added new module to render each leaderboard entry

--- src/components/library/Leaderboard.js
+++ src/components/library/Leaderboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { addProfileToEntries, getEntriesByRudiment } from "../data_management/EntriesData.js";
 import { getStudentProfiles } from "../data_management/StudentsProfileData.js";
+import { LeaderboardEntries } from "./LeaderboardEntries.js";
 
 export const Leaderboard = ({ rudiment }) => {
     const [entries, setEntries] = useState([]);
@@ -14,5 +15,14 @@ export const Leaderboard = ({ rudiment }) => {
         );
     });
 
-    return <></>;
+    return (
+        <section>
+            <h3>Leaderboard</h3>
+            <ol>
+                {entries.map((entry) => (
+                    <LeaderboardEntries entry={entry} />
+                ))}
+            </ol>
+        </section>
+    );
 };
