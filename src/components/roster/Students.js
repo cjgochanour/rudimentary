a02a56ecd468152adc0583b7eb0452cd89238ac1
cjgochanour <cjgochanour@gmail.com>
2022-02-01 18:11:35 -0600

    added student.js module for rendering a student in the list

--- src/components/roster/Students.js
+++ src/components/roster/Students.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getInstructorsStudents } from "../data_management/StudentsProfileData.js";
+import { Student } from "./Student.js";
 
 export const Students = () => {
     const [instructorStudents, setStudents] = useState([]);
@@ -11,7 +12,11 @@ export const Students = () => {
     return (
         <>
             <h1>Roster</h1>
-            {instructorStudents.map((student) => {})}
+            <ul>
+                {instructorStudents.map((student) => (
+                    <Student key={student.id} student={student} />
+                ))}
+            </ul>
         </>
     );
 };
