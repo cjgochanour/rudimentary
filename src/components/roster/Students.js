1a86172a27cceb45fa1dfd0d8da88560a6483705
cjgochanour <cjgochanour@gmail.com>
2022-02-01 18:00:31 -0600

    state now sets for the instructor's students

--- src/components/roster/Students.js
+++ src/components/roster/Students.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getInstructorsStudents } from "../data_management/StudentsProfileData.js";
 
 export const Students = () => {
+    const [instructorStudents, setStudents] = useState([]);
+
+    useEffect(() => {
+        getInstructorsStudents().then((studentsArray) => setStudents(studentsArray));
+    }, []);
+
     return <></>;
 };
