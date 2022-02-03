f67a369809fff7e908283649c585c7dfa6a14c62
cjgochanour <cjgochanour@gmail.com>
2022-02-03 09:34:56 -0600

    checkbox is checked by default if studentsProfile object has leaderboard access

--- src/components/roster/LeaderboardAccess.js
+++ src/components/roster/LeaderboardAccess.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const LeaderboardAccess = ({ student, stateSetter }) => {
-    return (
-        <>
-            <label htmlFor="lbBox">Leaderboard Access</label>
-            <input type="checkbox" />
-        </>
-    );
-};
+export const LeaderboardAccess = ({ student, stateSetter }) => (
+    <>
+        <label htmlFor="lbBox">
+            Leaderboard Access
+            <input type="checkbox" checked={student.leaderboardAccess} />
+        </label>
+    </>
+);
